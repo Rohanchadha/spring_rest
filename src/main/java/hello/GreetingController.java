a7c64459d581156935dad719ec897bbfedeb1da8
nikhil suri <nikhil.suri@myntra.com>
2019-01-17 00:03:50 +0530

    2nd change temp

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="Bro") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="hello") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
